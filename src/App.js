eff6c47e5a147e333594424909b3af4b86de6a60
wbcook <wbcook007@gmail.com>
2017-01-04 22:57:53 -0500

    AppIntro.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppIntro from './AppIntro';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,9 +20,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hello, {formatName(user)}!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppIntro />
       </div>
     );
   }
---
+++ src/AppIntro.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class AppIntro extends Component {
+  render() {
+    return (
+      <div className="AppIntro">
+        <p className="App-intro">
+          To get started, edit <code>HFJASHFiewjhiofjdkljfklwjf</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AppIntro;
