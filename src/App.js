5b5c1e13c289d4e03144775a4a88f84feacfa222
wbcook <wbcook007@gmail.com>
2017-01-04 22:26:33 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Will',
+  lastName: 'Cook'
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Hello, {formatName(user)}!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
