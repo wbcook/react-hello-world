3e557fe5ca2f7416a38cec2af52e8f223907e9a7
wbcook <wbcook007@gmail.com>
2017-01-07 02:39:01 -0500

    Updates

--- src/AppIntro.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppIntro from './AppIntro';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppIntro />, div);
-});
--- src/Clock.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Clock from './Clock';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Clock />, div);
-});
