9a58314548b0b0a234376bb54621171342912901
wbcook <wbcook007@gmail.com>
2017-01-10 02:41:28 -0500

    SendMail Subject

--- src/SendMail.js
+++ src/SendMail.js
@@ -5,19 +5,25 @@ class SendMail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'Enter your message here, then click send.'
+      subject: 'No Subject',
+      message: 'Enter your message here, then click send.'
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleSubjectChange = this.handleSubjectChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleMessageChange(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubjectChange(event) {
+    this.setState({subject: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('Your message was sent: ' + this.state.value);
+    alert('Your message was sent: \n' + this.state.subject + ': \n' + this.state.message);
     event.preventDefault();
   }
 
@@ -28,7 +34,16 @@ class SendMail extends Component {
           <form onSubmit={this.handleSubmit}>
             <label>
               Name:
-              <textarea value={this.state.value} onChange={this.handleChange} />
+              <textarea value={this.state.message} onChange={this.handleMessageChange} />
+            </label>
+            <label>
+              Pick a subject for your message:
+              <select value={this.state.subject} onChange={this.handleSubjectChange}>
+                <option value="React">React</option>
+                <option value="Redux">Redux</option>
+                <option value="JSX">JSX</option>
+                <option value="Rick and Morty">Rick and Morty</option>
+              </select>
             </label>
             <input type="submit" value="Send" />
           </form>
